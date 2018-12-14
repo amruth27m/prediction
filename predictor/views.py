97469b8dae5c93236485dadcc7137e3b4af4b348
Amruth Lal <amruth27m@gmail.com>
2018-12-14 11:20:19 +0530

    Refractor API endpoint code

--- predictor/views.py
+++ predictor/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint,make_response,jsonify
 from flask_restful import Api,Resource,reqparse
-from utils.constants import PROCEDURES,SEX,AGE_MIN,AGE_MAX,INR_MIN,INR_MAX
+from utils.constants import PROCEDURES,GENDER,AGE_MIN,AGE_MAX,INR_MIN,INR_MAX,PATIENT_DATA_PARAMETERS
 
 predictor = Blueprint('predictor', __name__)
 
@@ -9,47 +9,69 @@ def index():
     return "Predictor Home stub",200
 
 def parseRequest(parser):
-    parser.add_argument("sex")
-    parser.add_argument("procedure")
-    parser.add_argument("age")
-    parser.add_argument("oldINRValue")
-    parser.add_argument("newINRValue")
-    parser.add_argument("oldDose")
+    for parameter in PATIENT_DATA_PARAMETERS:
+        parser.add_argument(parameter)
     return parser.parse_args()
 
 def validateArgs(args):
-
-    age = int(args["age"])
-    if age<=AGE_MIN or age>AGE_MAX:
-        return False
+    '''Returns true is the args is valid patient data parameters
+        else throws Exception
+    '''
+    try:
+        age = int(args["age"])
+        if age <= AGE_MIN or age > AGE_MAX:
+            raise ValueError("age must be between %d and %d" % (AGE_MIN,AGE_MAX))
+    except ValueError as error:
+        raise ValueError(error)
 
     procedure = args["procedure"]
     if procedure not in PROCEDURES:
-        return False
+        raise ValueError("Undefined procedure %s" % procedure)
 
-    sex = args["sex"]
-    if sex not in SEX:
-        return False
+    gender = args["gender"]
+    if gender not in GENDER:
+        raise ValueError("Undefined gender %s" % gender)
 
-    oldINRValue = float(args["oldINRValue"])
-    if oldINRValue<=INR_MIN or oldINRValue>INR_MAX:
-        return False
+    try:
+        oldINRValue = float(args["oldINRValue"])
+        if oldINRValue<=INR_MIN or oldINRValue>INR_MAX:
+            raise ValueError("oldINRValue must be between %d and %d" %(INR_MIN, INR_MAX))
+    except TypeError as error:
+        raise ValueError("oldINRValue cannot be %s \n %s" %(args["oldINRValue"],error))
 
-    newINRValue = float(args["newINRValue"])
-    if newINRValue<=INR_MIN or newINRValue>INR_MAX:
-        return False
+    try:
+        newINRValue = float(args["newINRValue"])
+        if newINRValue<=INR_MIN or newINRValue>INR_MAX:
+            raise ValueError("oldINRValue must be between %d and %d" % (INR_MIN, INR_MAX))
+    except TypeError as error:
+        raise ValueError("newINRValue cannot be %s \n %s"%(args['newINRValue'],error))
 
     return True
 
-@predictor.route('/predictor/validate/',methods=['POST'])
+@predictor.route('/predictor/validate/',methods=['POST','GET'])
 def validate():
     parser = reqparse.RequestParser()
     args = parseRequest(parser)
-    if(validateArgs(args)):
-        return make_response(jsonify({
-            "validation": "success"
-        }))
-    else:
-        return make_response(jsonify({
-            "validation": "failed"
-        }))
+    response = {}
+    try:
+        response["validation"] = validateArgs(args)
+    except (TypeError,ValueError) as error:
+        response["validation"] = False
+    finally:
+        return make_response(jsonify(response))
+
+
+@predictor.route('/predictor/predict/',methods=['POST'])
+def predict():
+    '''Stub for prediction'''
+    parser = reqparse.RequestParser()
+    args = parseRequest(parser)
+    respone = {}
+    try:
+        respone["validation"] = validateArgs(args)
+        respone["predicted_dosage"] = True
+    except (TypeError,ValueError) as error:
+        respone["validation"] = False
+        respone["predicted_dosage"] = False
+    finally:
+        return make_response(jsonify(respone))
--- utils/constants.py
+++ utils/constants.py
@@ -1,5 +1,6 @@
 PROCEDURES = ["MVR", "AVR", "DVR", "AF"]
-SEX = ["Male", "Female", "Other"]
+GENDER = ["Male", "Female", "Other"]
+PATIENT_DATA_PARAMETERS = ["gender","procedure","age","oldINRValue","newINRValue","oldDose"]
 AGE_MIN = 0
 AGE_MAX = 120
 INR_MIN = 2
